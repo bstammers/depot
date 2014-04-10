c16ecd28c55b636f95c34193b22e45ff8ba5eda5
Brad Stammers <brad.stammers@iinet.net.au>
2014-04-10 16:47:30 +1000

    Chapter 11

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Depot::Application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :orders
+
   get "store/index"
   resources :products
-
+  
   root 'store#index', as: 'store'
   
   # The priority is based upon order of creation: first created -> highest priority.
